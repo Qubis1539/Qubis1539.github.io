d46242c6e7ba4801f066a06e0f3195be4a99ea3a
Michail <wb_qs@mail.ru>
2022-12-03 14:24:30 +0300

    add active effects

--- pages/menu test/scripts/script.js
+++ pages/menu test/scripts/script.js
@@ -4,7 +4,8 @@ let burger = document.querySelector(".hamburger")
 let navMenu = document.querySelector("#nav_menu")
 let links = document.querySelectorAll(".nav_link")
 
-
+let activeLinks = document.querySelectorAll(".main_link")
+let mainLinkId = 0
 
 
 burger.addEventListener("click", function() {
@@ -12,9 +13,17 @@ burger.addEventListener("click", function() {
     burger.classList.toggle("hamburger_active")
 })
 
-links.forEach(function(item) {
-    item.addEventListener("click", function() {
-        navMenu.classList.add("nav-hide");
-        burger.classList.remove("hamburger_active")
+// links.forEach(function(item) {   
+//     item.addEventListener("click", function() {
+//         navMenu.classList.add("nav-hide");
+//         burger.classList.remove("hamburger_active")
+//     })
+// })
+
+activeLinks.forEach((item, id) => {
+    item.addEventListener("click", () => {
+        activeLinks[mainLinkId].classList.remove("active");
+        item.classList.add("active");
+        mainLinkId = id
     })
 })
